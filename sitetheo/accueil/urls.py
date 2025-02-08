5e51bd566fa8aeb9d46e869499affb04a8e54fa8
Theo <theofontana@orange.fr>
2025-02-08 13:05:30 +0100

    First deploy OK

--- sitetheo/accueil/urls.py
+++ sitetheo/accueil/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, cv_detail
 
 urlpatterns = [
     path('', home, name='home'),
+    path('cv/', cv_detail, name='cv_detail'),
+
 ]
